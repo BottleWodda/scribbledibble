0feed8ae2abfbbb7f3b63258f7d2fbe4ca801a4f
Jake Lyell <jakelyell7@gmail.com>
2022-04-06 21:32:36 +1000

    Created basic grid generator for etch a sketch project

---
+++ javascript.js
@@ -0,0 +1,25 @@
+function makeGrid (row, col) {
+    if (row > 50 || col > 50) {
+        return alert('Your choice was too high, idiot.');
+    }
+    for (let i = 0; i <row; i++) {    //creates a div for number in 'row' parameter
+        let row = document.createElement('div');
+        row.className = 'row';
+        for (let j = 0; j < col; j++) {     //creates a div for number in 'col' parameter
+            let column = document.createElement('div');
+            column.className = 'column';
+            row.appendChild(column);
+            column.addEventListener('mouseover', () => {
+                column.style.backgroundColor = 'black';
+            });
+        }
+        document.getElementById('container').appendChild(row);
+    }
+}
+
+function reset() {
+    location.reload();
+}
+
+makeGrid(prompt('Choose width. (Max 50)'), prompt('Choose height. (Max 50)'));
+
