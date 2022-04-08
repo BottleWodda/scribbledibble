426823ccf385a191de8067819da93c3f86c66036
Jake Lyell <jakelyell7@gmail.com>
2022-04-08 22:51:23 +1000

    Added rainbow feature. Still missing dynamic cells and 1/10 black tile

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
+//User inputs canvas size and draws in black
 function createGrid(row, col) {
     row = col = prompt('Enter grid size from 2-100.')
 
-    for (let i = 0; i <row; i++) {    //creates a div for number in 'row' parameter
+    for (let i = 0; i <row; i++) {
         let row = document.createElement('div');
         row.className = 'row';
         row.id = 'row';
 
-        for (let j = 0; j < col; j++) {     //creates a div for number in 'col' parameter
+        for (let j = 0; j < col; j++) {
             let column = document.createElement('div');
             column.className = 'column';
             column.id = 'column';
@@ -19,6 +20,7 @@ function createGrid(row, col) {
     }
 }
 
+//Erases all drawing and user inputted grid
 function newCanvas() {
     const erase = document.getElementById('gridContainer');
     while (erase.hasChildNodes()) {
@@ -26,10 +28,44 @@ function newCanvas() {
     }
 }
 
-function rainbow() {
-
+function oneInTenBlack() {
 }
 
-function oneInTenBlack() {
 
+//creates random selection of rgb colours
+function colors() {
+    let colorArray = [];    
+  
+    for(let i =0; i < 3 ; i++){
+      colorArray.push(Math.floor(Math.random() * (255 - 0) + 0));
+    }
+    // rgb -> hex
+    let color = colorArray
+      .map( x => x.toString(16))
+      .join('');
+
+    return `#${color}`;
+  }
+
+  //User inputs canvas size and draws in random RGB
+  function rainbow(row, col) {
+    alert('Rainbow Mode Activated');
+    row = col = prompt('Enter grid size from 2-100.')
+
+    for (let i = 0; i <row; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        row.id = 'row';
+
+        for (let j = 0; j < col; j++) {
+            let column = document.createElement('div');
+            column.className = 'column';
+            column.id = 'column';
+            row.appendChild(column);
+            column.addEventListener('mouseover', () => {
+                column.style.backgroundColor = colors();
+            });
+        }
+        document.getElementById('gridContainer').appendChild(row);
+    }
 }
