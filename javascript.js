c22f0b95a76cc26aa96516e87531c3a08ba283f8
Jake Lyell <jakelyell7@gmail.com>
2022-04-08 21:24:47 +1000

    Added working erase button. 'New Canvas' button now erases board then calls in new board size. Still need to make grid dynamic

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 function createGrid(row, col) {
-    //row = col = prompt('Enter grid size from 2-100.')
+    row = col = prompt('Enter grid size from 2-100.')
 
     for (let i = 0; i <row; i++) {    //creates a div for number in 'row' parameter
         let row = document.createElement('div');
         row.className = 'row';
+        row.id = 'row';
+
         for (let j = 0; j < col; j++) {     //creates a div for number in 'col' parameter
             let column = document.createElement('div');
             column.className = 'column';
+            column.id = 'column';
             row.appendChild(column);
             column.addEventListener('mouseover', () => {
                 column.style.backgroundColor = 'black';
@@ -17,7 +20,10 @@ function createGrid(row, col) {
 }
 
 function newCanvas() {
-
+    const erase = document.getElementById('gridContainer');
+    while (erase.hasChildNodes()) {
+        erase.removeChild(erase.firstChild)
+    }
 }
 
 function rainbow() {
@@ -27,5 +33,3 @@ function rainbow() {
 function oneInTenBlack() {
 
 }
-
-createGrid(40, 40);
