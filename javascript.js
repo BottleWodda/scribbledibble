29c9d29386f87b2f1291104b31e0e702d19717cc
Jake Lyell <jakelyell7@gmail.com>
2022-04-09 16:48:07 +1000

    Added click to draw function

--- javascript.js
+++ javascript.js
@@ -28,9 +28,29 @@ function newCanvas() {
     }
 }
 
-function resizeGrid() {
-    
+function clickToDraw(row, col) {
+    alert('Click to draw enabled.')
+    row = col = prompt('Enter grid size from 2-100.')
+
+    for (let i = 0; i <row; i++) {
+    let row = document.createElement('div');
+    row.className = 'row';
+    row.id = 'row';
+
+    for (let j = 0; j < col; j++) {
+        let column = document.createElement('div');
+        column.className = 'column';
+        column.id = 'column';
+        row.appendChild(column);
+        column.addEventListener('click', () => {
+            column.style.backgroundColor = 'black';
+        });
+    }
+    document.getElementById('gridContainer').appendChild(row);
+    }
 }
+
+
 
 //creates random selection of rgb colours
 function colors() {
